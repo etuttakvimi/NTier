ecd61f77a7db94e345f82ccecd7837e6f3ae9625
muratvuranok <mrtvrnk@gmail.com>
2019-05-16 12:45:19 +0300

    ADUsername düzenlendi

--- NTier.Core/Entities/CoreEntity.cs
+++ NTier.Core/Entities/CoreEntity.cs
@@ -9,15 +9,14 @@ namespace NTier.Core.Entities
         public DateTime CreatedDate { get; set; }
         public string CreatedComputerName { get; set; }
         public string CreatedIP { get; set; }
-        public Guid CreatedBy { get; set; }
-
-        public string CreatedAADUsername { get; set; }
+        public Guid CreatedBy { get; set; } 
+        public string CreatedADUsername { get; set; }
 
         public DateTime? ModifiedDate { get; set; }
         public string ModifiedComputerName { get; set; }
         public string ModifiedIP { get; set; }
         public Guid? ModifiedBy { get; set; }
-        public string ModifiedCreatedAADUsername { get; set; }
+        public string ModifiedADUsername { get; set; }
 
     }
 }
