bbaea68d548a1be67ed3d581a91b701f1189f385
muratvuranok <mrtvrnk@gmail.com>
2019-05-16 12:29:00 +0300

    Core Entity eklendi

---
+++ NTier.Core/Entities/CoreEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NTier.Core.Entities
+{
+    public class CoreEntity : ICoreEntity
+    {
+        public Guid Id { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public string CreatedComputerName { get; set; }
+        public string CreatedIP { get; set; }
+        public Guid CreatedBy { get; set; }
+
+
+        public DateTime? ModifiedDate { get; set; }
+        public string ModifiedComputerName { get; set; }
+        public string ModifiedIP { get; set; }
+        public Guid? ModifiedBy { get; set; }
+
+    }
+}
